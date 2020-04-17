3cfee6d62474be13c0d5fc7238d8479518ad720f
Saed Mami <saed.mami@gmail.com>
2020-04-17 11:34:47 +0100

    Setup react router for dashboard and addProject

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header></Header>
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
